7b60dedd369c8ffac4345551f2b39f66a3addcf3
gelborn <dwarf11@gmail.com>
2020-07-24 15:37:11 +0300

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,6 +26,24 @@
         </div>
     </div>
 
-    
+@endsection
 
+@section('pizza')
+    <div class="pizza-container">
+
+        @foreach ($products as $product)
+
+             <div class="pizza-box">
+                 <img class="img-box" src=" {{ asset('images/'.$product->images)}}" alt="{{$product->images}}">
+                 <p class="name-box">{{$product->title}}</p>
+                 <p class="desciption-box">{{$product->description}}</p>
+                 <div class="price-box">
+                    Nuo {{$product->price}} €
+                    <button class="default-button">Pasirinkti</button>
+                 </div>
+             </div>
+            
+        @endforeach
+
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -49,36 +50,11 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                            @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-
-                                </div>
-                            </li>
-                        @endguest
+ 
+                        <li class="nav-item">
+                            <div class="default-button">Krepšelis</div>
+                        </li>
+            
                     </ul>
                 </div>
             </div>
@@ -127,6 +103,9 @@
         <!-- Prideta -->
         <main class="py-4">
             @yield('content')
+        </main>
+        <main class="py-4">
+            @yield('pizza')
         </main>
     </div>
 </body>
